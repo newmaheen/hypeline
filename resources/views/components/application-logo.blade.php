5d3e67e9ffcd78cf5630150640d5637f8e56d7ab
newmaheen <newmaheen@gmail.com>
2026-09-17 13:18:03 +0600

    fix: enforce custom bootstrap register layout and remove guest component

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,7 @@
-<img src="{{ asset('images/hypeline-logo-2.png') }}" alt="Hypeline Logo" class="h-16 w-auto object-contain mx-auto" {{ $attributes }}>
+<div style="display: flex; justify-content: center; align-items: center; width: 100%; margin-bottom: 1rem;">
+    <img 
+        src="{{ asset('images/hypeline-logo-2.png') }}" 
+        alt="Hypeline Logo" 
+        style="width: 70px; height: 70px; max-width: 70px; max-height: 70px; object-fit: contain; display: block;"
+    >
+</div>
